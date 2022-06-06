fb9fdd92a956c097380627ab701ffc341815d09f
Humayan Kabir <humayan.kabir94@gmail.com>
2022-06-06 11:21:38 +0600

    added a new line.

--- script.py
+++ script.py
@@ -2,3 +2,5 @@ numberOfSlices = int(input())
 tasteRating = input()
 
 print("The inputs are: ", numberOfSlices, ", ", tasteRating)
+
+print("The inputs are: " + str(numberOfSlices) + ", " + tasteRating)
