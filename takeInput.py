e2a48761355764d0b961dab89e2e700a395db1f9
sanjibpaul59 <sanjibpaul171@gmail.com>
2022-06-06 11:17:19 +0600

    removed conflict

--- takeInput.py
+++ takeInput.py
@@ -1,8 +1,4 @@
 userInput = input("Enter a number: ")
 print(int(userInput) + 2)
-<<<<<<< HEAD
-print(int(userInput)+1)
-=======
-print(int(userInput))
+print(int(userInput) + 1)
 print(userInput)
->>>>>>> 941deee5bc931538da2029b709c95fde6af63bb6
